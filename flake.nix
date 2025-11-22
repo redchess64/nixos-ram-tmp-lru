b99114c2e264787adfde5ce009d9cb16b186cb2c
redchess64 <155838822+redchess64@users.noreply.github.com>
2025-11-21 21:13:51 -0500

    Clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "NixOS RAM tmpfs LRU cleaner";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
-  outputs = { self, nixpkgs, ... }: {
+  outputs = _: {
     nixosModules.tmp-lru-tmp = import ./modules/tmp-lru-tmp.nix;
   };
 }
